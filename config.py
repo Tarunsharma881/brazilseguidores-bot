3f24b4a2ca62960a8649f17a1a312b0e08003b3d
Tarunsharma881 <65032584+Tarunsharma881@users.noreply.github.com>
2025-12-15 07:09:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 LANGUAGE = os.getenv("LANGUAGE", "pt")
+SUPPORT_WHATSAPP = os.getenv("SUPPORT_WHATSAPP", "")
+
